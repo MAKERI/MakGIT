f1bd2021f19848da4e6b5e708f701e6bb66549fb
pknowledge <mohamed.makeri@gmail.com>
2017-05-13 01:13:49 +0200

    [DEV][fix] ajout de class unit

---
+++ deploiementAnt/src/Addition.java
@@ -0,0 +1,16 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	 public Long calculer(long a, long b) {
+		 long c=a+b;
+	      return new Long(c);
+	 }
+	 
+	 public Character lireSymbole() {
+	      return new Character('-');
+	   }
+}
